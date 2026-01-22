2625ee9d165ee163e0a00d9a1bac6408f7711712
SOHUMks149 <11249A149@kanchiuniv.ac.in>
2026-01-22 12:36:18 +0530

    Update 11249a149_exp1b.c

--- 11249a149_exp1b.c
+++ 11249a149_exp1b.c
@@ -2,13 +2,11 @@
 
 // Recursive function to get the n-th Fibonacci number
 int fibonacci(int n) {
-    // Base cases: the first two terms are 0 and 1
     if (n == 0) {
         return 0;
     } else if (n == 1) {
         return 1;
     }
-    // Recursive case: the n-th term is the sum of the two preceding ones
     else {
         return (fibonacci(n - 1) + fibonacci(n - 2));
     }
@@ -16,8 +14,6 @@ int fibonacci(int n) {
 
 int main() {
     int n, i;
-
-    // Get the number of terms from the user
     printf("Enter the number of terms: ");
     scanf("%d", &n);
 
@@ -25,7 +21,7 @@ int main() {
         printf("Fibonacci series is not possible for negative terms.\n");
     } else {
         printf("Fibonacci Series: ");
-        // Loop from 0 to n-1 to print each term
+        
         for (i = 0; i < n; i++) {
             printf("%d ", fibonacci(i));
         }
@@ -34,4 +30,5 @@ int main() {
 
     return 0;
 }
+
 
