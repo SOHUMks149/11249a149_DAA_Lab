b20544ba96ec205310554e659ab18bf73be70c58
SOHUMks149 <11249A149@kanchiuniv.ac.in>
2026-01-29 13:08:14 +0530

    Update 11249a149_exp1b.c

--- 11249a149_exp1b.c
+++ 11249a149_exp1b.c
@@ -16,19 +16,11 @@ int main() {
     int n, i;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
-
-    if (n < 0) {
-        printf("Fibonacci series is not possible for negative terms.\n");
-    } else {
-        printf("Fibonacci Series: ");
-        
-        for (i = 0; i < n; i++) {
-            printf("%d ", fibonacci(i));
-        }
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
         printf("\n");
     }
-
-    return 0;
-}
 
 
